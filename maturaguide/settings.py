52e329774f9be03dbf7d705766fd620e5c1db9cf
Jebok <kanakuba123359@gmail.com>
2021-10-15 01:22:25 +0200

    Some improvements on site and add responsive navbar

--- maturaguide/settings.py
+++ maturaguide/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-329lrtpv&6=-9b3gce)w--xmk1%@j__8jterd!^cl*48mi87y4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*",]
 
 
 # Application definition
--- static/script/main.js
+++ static/script/main.js
@@ -6,6 +6,42 @@ function bindAllActions() {
     $(window).scroll(updateNavHighlight);
 }
 
+class ResponsiveNavBar{
+    constructor(){
+        this.navbar = $("nav")
+        this.navbarBtn = $("nav img#responsive-button")
+        this.navbarMenu = $("div.nav-menu")
+        this.navbarShowed = false;
+        this.connectButton();
+    }
+
+    connectButton(){
+        let thisObj = this
+        this.navbarBtn.click(() => {
+            this.onNavbarBtnClick.call(thisObj);
+        });
+        $("div.nav-menu a.nav-link").click(() => {
+            this.onNavbarBtnClick.call(thisObj);
+        });
+    }
+
+    onNavbarBtnClick(action){
+        if (this.navbarShowed) {
+            this.navbarBtn.attr("src", "/static/img/responsive-btn.svg");
+            this.navbarMenu.animate({top: "-100%"}, "fast", () => {
+                this.navbarMenu.css("display", "none")
+            })
+            this.navbarShowed = false;
+        }else{
+            this.navbarBtn.attr("src", "/static/img/close.svg");
+            this.navbarMenu.css("display", "flex")
+            this.navbarMenu.animate({top: "0%"}, "fast")
+            this.navbarShowed = true;
+        }
+    }
+}
+
 (() => {
     bindAllActions();
+    let rn = new ResponsiveNavBar()
 })()
