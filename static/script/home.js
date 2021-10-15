78bf92177e68fca27e687a2fdb08fe9f37843c43
Jebok <kanakuba123359@gmail.com>
2021-10-15 22:14:30 +0200

    Format home.js

--- static/script/home.js
+++ static/script/home.js
@@ -62,117 +62,129 @@ class QuizPicker {
   }
 
   bindButtons() {
-    $("div.category").unbind("click")
+    $("div.category").unbind("click");
     let thisObj = this;
     $("div.category").click((event) => {
-        let choiceId = undefined;
-        switch (thisObj.mode) {
-            case 0: // Subject
-            thisObj.pickedSubject = event.currentTarget.id;
+      let choiceId = undefined;
+      switch (thisObj.mode) {
+        case 0: // Subject
+          thisObj.pickedSubject = event.currentTarget.id;
+          thisObj.mode = 1;
+          thisObj.doTransition(event.currentTarget, () => {
+            thisObj.showTypeCards(), thisObj.bindButtons();
+          });
+          break;
+        case 1: // Type
+          choiceId = event.currentTarget.id;
+          if (choiceId == 0) {
+            thisObj.mode = 0;
+            thisObj.pickedSubject = undefined;
+            thisObj.doTransition(
+              event.currentTarget,
+              () => {
+                thisObj.showSubjectsCards().then(() => {
+                  thisObj.bindButtons();
+                });
+              },
+              true
+            );
+          } else {
+            thisObj.mode = 2;
+            thisObj.pickedMode = choiceId;
+            if (choiceId == 1) {
+              // Trening
+              thisObj.doTransition(event.currentTarget, () => {
+                thisObj.showTraningCards(thisObj.pickedSubject).then(() => {
+                  thisObj.bindButtons();
+                });
+              });
+            } else {
+              thisObj.doTransition(event.currentTarget, () => {
+                thisObj.showSheetCards(thisObj.pickedSubject).then(() => {
+                  thisObj.bindButtons();
+                });
+              });
+            }
+          }
+          break;
+        case 2: // Exam / Category picker
+          choiceId = event.currentTarget.id;
+          if (choiceId == 0) {
             thisObj.mode = 1;
-            thisObj.doTransition(event.currentTarget, () => {
-                thisObj.showTypeCards(),
+            thisObj.pickedMode = undefined;
+            thisObj.doTransition(
+              event.currentTarget,
+              () => {
+                thisObj.showTypeCards();
                 thisObj.bindButtons();
-            });
-            break;
-            case 1: // Type
-            choiceId = event.currentTarget.id;
-            if (choiceId == 0) {
-                thisObj.mode = 0;
-                thisObj.pickedSubject = undefined;
-                thisObj.doTransition(
-                    event.currentTarget,
-                    () => {
-                        thisObj.showSubjectsCards().then(() => {
-                            thisObj.bindButtons();
-                        });
-                    },
-                    true
-                    );
-                }else{
-                    thisObj.mode = 2;
-                    thisObj.pickedMode = choiceId;
-                    if (choiceId == 1) { // Trening
-                        thisObj.doTransition(event.currentTarget, () => {
-                            thisObj.showTraningCards(thisObj.pickedSubject).then(() => {
-                                thisObj.bindButtons();
-                            });
-                        })
-                    }else{
-                        thisObj.doTransition(event.currentTarget, () => {
-                            thisObj.showSheetCards(thisObj.pickedSubject).then(() => {
-                                thisObj.bindButtons();
-                            });
-                        })
-                    }
-                }
-                break;
-                case 2: // Exam / Category picker
-                choiceId = event.currentTarget.id;
-                if (choiceId == 0) {
-                    thisObj.mode = 1;
-                    thisObj.pickedMode = undefined;
-                    thisObj.doTransition(event.currentTarget, () => {
-                        thisObj.showTypeCards();
-                        thisObj.bindButtons();
-                    }, true)
-                }else{
-                    thisObj.pickedCategory = choiceId; 
-                    window.location.href = `/play?subject=${thisObj.pickedSubject}&${thisObj.pickedMode == 1 ? "category=" + thisObj.pickedCategory : "cke_year=" + thisObj.pickedCategory}`
-                }
-                }
-            });
-        }
-        
-        showTypeCards(){
-            let htmlToAdd = "";
-            [
-                { id: 0, content: "wróć" },
-                { id: 1, content: "trening" },
-                { id: 2, content: "arkusz" },
-            ].forEach((element) => {
-                htmlToAdd += this.getTypeCardHTMLFromData(element);
+              },
+              true
+            );
+          } else {
+            thisObj.pickedCategory = choiceId;
+            window.location.href = `/play?subject=${thisObj.pickedSubject}&${
+              thisObj.pickedMode == 1
+                ? "category=" + thisObj.pickedCategory
+                : "cke_year=" + thisObj.pickedCategory
+            }`;
+          }
+      }
+    });
+  }
+
+  showTypeCards() {
+    let htmlToAdd = "";
+    [
+      { id: 0, content: "wróć" },
+      { id: 1, content: "trening" },
+      { id: 2, content: "arkusz" },
+    ].forEach((element) => {
+      htmlToAdd += this.getTypeCardHTMLFromData(element);
     });
     $("div.category-holder").html(htmlToAdd);
     $("h1#category-header").html("Wybierz typ nauki");
   }
 
-  async showTraningCards(subject_id){
-      const getTypeCardHTMLFromData = this.getTypeCardHTMLFromData
-      return $.getJSON(`/api/get_categories?subject_id=${subject_id}`, "",
-          function (data, textStatus, jqXHR) {
-              let htmlToAdd = getTypeCardHTMLFromData({id: 0, content: "wróć"});
-                data.forEach((element) => {
-                    // Reformat
-                    element = {
-                        id: element["id"],
-                        content: element["name"], 
-                    }
-                    htmlToAdd += getTypeCardHTMLFromData(element)
-              })
-              $("div.category-holder").html(htmlToAdd);
-              $("h1#category-header").html("Wybierz kategorie");      
-          }
-      );
+  async showTraningCards(subject_id) {
+    const getTypeCardHTMLFromData = this.getTypeCardHTMLFromData;
+    return $.getJSON(
+      `/api/get_categories?subject_id=${subject_id}`,
+      "",
+      function (data, textStatus, jqXHR) {
+        let htmlToAdd = getTypeCardHTMLFromData({ id: 0, content: "wróć" });
+        data.forEach((element) => {
+          // Reformat
+          element = {
+            id: element["id"],
+            content: element["name"],
+          };
+          htmlToAdd += getTypeCardHTMLFromData(element);
+        });
+        $("div.category-holder").html(htmlToAdd);
+        $("h1#category-header").html("Wybierz kategorie");
+      }
+    );
   }
 
-  async showSheetCards(subject_id){
-      const getTypeCardHTMLFromData = this.getTypeCardHTMLFromData
-      return $.getJSON(`/api/get_cke_sheets?subject_id=${subject_id}`, "",
-          function (data, textStatus, jqXHR) {
-              let htmlToAdd = getTypeCardHTMLFromData({id: 0, content: "wróć"});;
-                data.forEach((element) => {
-                    // Reformat
-                    element = {
-                        id: element,
-                        content: element, 
-                    }
-                    htmlToAdd += getTypeCardHTMLFromData(element)
-              })
-              $("div.category-holder").html(htmlToAdd);
-              $("h1#category-header").html("Wybierz rok");      
-          }
-      );
+  async showSheetCards(subject_id) {
+    const getTypeCardHTMLFromData = this.getTypeCardHTMLFromData;
+    return $.getJSON(
+      `/api/get_cke_sheets?subject_id=${subject_id}`,
+      "",
+      function (data, textStatus, jqXHR) {
+        let htmlToAdd = getTypeCardHTMLFromData({ id: 0, content: "wróć" });
+        data.forEach((element) => {
+          // Reformat
+          element = {
+            id: element,
+            content: element,
+          };
+          htmlToAdd += getTypeCardHTMLFromData(element);
+        });
+        $("div.category-holder").html(htmlToAdd);
+        $("h1#category-header").html("Wybierz rok");
+      }
+    );
   }
 
   async showSubjectsCards() {
